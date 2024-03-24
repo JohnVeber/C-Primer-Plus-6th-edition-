1d90e3c898c796ab9b962ea2d2bfa2d2afee3246
John Veber <evgeniy.weber@gmail.com>
2024-03-24 22:40:11 +0300

    Try to find the reason of mistake during compilation.

--- Lesson 2/Exercise_1.c
+++ Lesson 2/Exercise_1.c
@@ -1,10 +1,10 @@
+// Ex.1
+
 #include <stdio.h>
-#include <locale.h>
-int main (void)
-{
-    setlocale(LC_ALL, ".UTF8"); // ранее тут было LC_ALL, "rusian"
+
+int main (void){
     printf("Evgeny Veber\n");
-    printf("Евгений\nВебер\n");
-    printf("Евгений Вебер\n");
+    printf("Evgeny\nVeber\n");
+    printf("Evgeny Veber\n");
     return 0;
-}
+} 
